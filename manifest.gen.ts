2e0874edb6762e88322e27e7af1fd15d65c06257
John Rodrigues <john.rodrigues008@gmail.com>
2024-03-22 04:04:37 -0300

    Alteração da sessão main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,24 +5,26 @@
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
-import * as $$$$$$1 from "./sections/Hero.tsx";
-import * as $$$$$$2 from "./sections/ItWorks.tsx";
-import * as $$$$$$6 from "./sections/myHero.tsx";
-import * as $$$$$$3 from "./sections/ParagraphList.tsx";
-import * as $$$$$$7 from "./sections/sections/ItWorks.tsx";
-import * as $$$$$$4 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$5 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/Header.tsx";
+import * as $$$$$$2 from "./sections/Hero.tsx";
+import * as $$$$$$3 from "./sections/ItWorks.tsx";
+import * as $$$$$$7 from "./sections/myHero.tsx";
+import * as $$$$$$4 from "./sections/ParagraphList.tsx";
+import * as $$$$$$8 from "./sections/sections/ItWorks.tsx";
+import * as $$$$$$5 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$6 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "deco-sites/honda-jdm/sections/Footer.tsx": $$$$$$0,
-    "deco-sites/honda-jdm/sections/Hero.tsx": $$$$$$1,
-    "deco-sites/honda-jdm/sections/ItWorks.tsx": $$$$$$2,
-    "deco-sites/honda-jdm/sections/myHero.tsx": $$$$$$6,
-    "deco-sites/honda-jdm/sections/ParagraphList.tsx": $$$$$$3,
-    "deco-sites/honda-jdm/sections/sections/ItWorks.tsx": $$$$$$7,
-    "deco-sites/honda-jdm/sections/SimpleTextList.tsx": $$$$$$4,
-    "deco-sites/honda-jdm/sections/Theme/Theme.tsx": $$$$$$5,
+    "deco-sites/honda-jdm/sections/Header.tsx": $$$$$$1,
+    "deco-sites/honda-jdm/sections/Hero.tsx": $$$$$$2,
+    "deco-sites/honda-jdm/sections/ItWorks.tsx": $$$$$$3,
+    "deco-sites/honda-jdm/sections/myHero.tsx": $$$$$$7,
+    "deco-sites/honda-jdm/sections/ParagraphList.tsx": $$$$$$4,
+    "deco-sites/honda-jdm/sections/sections/ItWorks.tsx": $$$$$$8,
+    "deco-sites/honda-jdm/sections/SimpleTextList.tsx": $$$$$$5,
+    "deco-sites/honda-jdm/sections/Theme/Theme.tsx": $$$$$$6,
   },
   "apps": {
     "deco-sites/honda-jdm/apps/decohub.ts": $$$$$$$$$$$0,
--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,11 +1,27 @@
 export interface Props {
-      title: string;
+      name?: string;
 }
 
-export default function Header({ title }: Props) {
+export default function Section({ name = 'Header' }: Props) {
       return (
             <header>
                   <h1> Este é o header do batman </h1>
             </header>
       )
 }
+
+// export interface Props {
+//     /**
+//      * @description The description of name.
+//      */
+//     name?: string;
+//   }
+  
+//   export default function Section({ name = "Capy" }: Props) {
+//     return (
+//       <div class="flex h-96 items-center justify-center mt-10">
+//         <p class="leading-10 text-6xl">{name}</p>
+//       </div>
+//     );
+//   }
+  
